2907cd7646caf88cfbc094e628922b91392ddebe
Yohan LAUNAY <yohan.launay2022@campus-eni.fr>
2023-12-05 10:29:33 +0100

    Start blog

---
+++ Blog/component/_menu.php
@@ -0,0 +1,16 @@
+<div class="collapse navbar-collapse d-flex justify-content-between w-100" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link <?= ;?>" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Rechercher</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">S'inscrire</a>
+        </li>
+    </ul>
+    <span class="navbar-text d-block">
+        <a href="">Se connecter</a>
+    </span>
+</div>
---
+++ Blog/index.php
@@ -0,0 +1,60 @@
+<?php
+$title = 'ENI ECOLE - TP Fil Rouge - Gestion des acticles';
+$desc = 'Apprenez PHP en créant et en gérant des articles en ligne';
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Blog</title>
+</head>
+<body>
+<main>
+    <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Blog</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <?php require_once 'component/_menu.php' ?>
+                </div>
+            </nav>
+        </header>
+        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+                <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+            </div>
+        </div>
+
+        <div class="row align-items-md-stretch">
+            <div class="col-md-6">
+                <div class="h-100 p-5 text-bg-dark rounded-3">
+                    <h2>Change the background</h2>
+                    <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+                    <button class="btn btn-outline-light" type="button">Example button</button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+                    <h2>Add borders</h2>
+                    <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+                    <button class="btn btn-outline-secondary" type="button">Example button</button>
+                </div>
+            </div>
+        </div>
+
+        <footer class="pt-3 mt-4 text-body-secondary border-top">
+            © 2023
+        </footer>
+    </div>
+</main>
+</body>
+</html>
